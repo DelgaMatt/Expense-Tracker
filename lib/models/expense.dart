e337ff1f182a9d7069b6ba355515a7b39a0e9f3a
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-10-23 19:52:56 -0700

    finished expense model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -3,17 +3,23 @@ import 'package:uuid/uuid.dart';
 // giving us the opportunity to generate unique id's
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
+// allows us to create a custom Type that is of predefined values
+// These aren't stored necessarily as string values but as a blanket value
 class Expense {
-  Expense(
-      {required this.title, 
-      required this.amount, 
-      required this.date,
-      }) : id = uuid.v4();
-      // will generate a String id - this is called an initializer list.
-      //can init class properties with values that are NOT recieved as constructor function arguments
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+  // will generate a String id - this is called an initializer list.
+  //can init class properties with values that are NOT recieved as constructor function arguments
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final String category;
 }
