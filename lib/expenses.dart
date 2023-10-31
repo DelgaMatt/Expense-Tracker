8a31d46411ea7b772ef53d7dadf95835b5a06275
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-10-30 20:22:28 -0700

    added modal overlay to AppBar

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -2,7 +2,6 @@ import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
-
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -15,29 +14,52 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
-      title: "Flutter Course", 
-      amount: 19.99, 
-      date: DateTime.now(), 
-      category: Category.work
-    ),
+        title: "Flutter Course",
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
     Expense(
-      title: "Gas", 
-      amount: 60, 
-      date: DateTime.now(), 
-      category: Category.travel
-    ),
+        title: "Gas",
+        amount: 60,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        title: "Cinema",
+        amount: 15,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: "Tacos",
+        amount: 25,
+        date: DateTime.now(),
+        category: Category.food)
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: ((context) => const Text('Modal Overlay')));
+  }
+  // doesnt return anything so return type is void
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay, 
+            icon: const Icon(Icons.add)
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),
           Expanded(
             child: ExpensesList(expenses: _registeredExpenses),
           )
-         
         ],
       ),
     );
