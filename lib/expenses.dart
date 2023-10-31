966c048cbc334bb31cf686c0e1b180c93c328832
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-10-30 21:55:01 -0700

    pop up modal ui complete

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -38,7 +39,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
         context: context,
-        builder: ((context) => const Text('Modal Overlay')));
+        builder: ((context) => const NewExpense()));
   }
   // doesnt return anything so return type is void
 
