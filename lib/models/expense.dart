bdf52e7dbeefb46f812a8e4a181c16ffec795c6d
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-10-31 18:09:41 -0700

    expense chart added

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -39,3 +39,26 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+// expense loop for chart
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category) 
+  : expenses = allExpenses
+    .where((expense) => expense.category == category)
+    .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
